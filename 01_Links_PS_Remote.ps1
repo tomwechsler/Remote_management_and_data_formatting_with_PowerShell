78ea1dc3fa54ce4a0ca3d83a536219f5bd5681af
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-07-25 07:33:32 +0200

    Add files via upload

---
+++ 01_Links_PS_Remote.ps1
@@ -0,0 +1,26 @@
+﻿#____________________________________________________________
+#https://www.winsolution.ch
+#https://www.youtube.com/channel/UCTNeHOi-dFeMLPkrYA9VbHw
+#____________________________________________________________
+
+#region WinRM Links
+
+#Running Remote Commands
+#https://docs.microsoft.com/en-us/powershell/scripting/learn/remoting/running-remote-commands
+
+#Windows Remote Management
+#https://docs.microsoft.com/en-us/windows/win32/winrm/portal
+
+#Installation and Configuration for Windows Remote Management
+#https://docs.microsoft.com/en-us/windows/win32/winrm/installation-and-configuration-for-windows-remote-management
+
+#Making the second hop in PowerShell Remoting
+#https://docs.microsoft.com/en-us/powershell/scripting/learn/remoting/ps-remoting-second-hop
+
+#PowerShell remoting over SSH
+#https://docs.microsoft.com/en-us/powershell/scripting/learn/remoting/ssh-remoting-in-powershell-core
+
+#How to configure WinRM for HTTPS manually
+#https://www.visualstudiogeeks.com/devops/how-to-configure-winrm-for-https-manually
+
+#endregion
