40a59e9c2ea8431d7176b83bd85a929f2f64460d
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-08-01 07:31:24 +0200

    Add files via upload

---
+++ 04_Creds.ps1
@@ -0,0 +1,21 @@
+﻿#Creating PSCredential Object using Get-Credential
+$creds = Get-Credential 
+
+$creds = Get-Credential –Credential "admin"
+
+#Create a Session
+Enter-PSSession -ComputerName 172.16.1.202 -Credential $creds
+
+#Create PSCredentialfrom Username and Secure Password
+$user = "admin"
+$pwd = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+
+$creds = New-Object System.Management.Automation.PSCredential($user, $pwd)
+
+#Create a Session
+Enter-PSSession -ComputerName 172.16.1.202 -Credential $creds
+
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user,$pwd
+
+#Create a Session
+Enter-PSSession -ComputerName 172.16.1.202 -Credential $creds
---
+++ 04_Trusted_Hosts.ps1
@@ -0,0 +1,15 @@
+﻿#Creating Trusted Host Entries
+
+#Create using a Command Prompt
+winrm set winrm/config/client @{TrustedHosts= "172.16.1.201"}
+
+#Create using PowerShell
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "172.16.1.201" –Force
+
+#Adding Trusted Host Entries using PowerShell
+$hosts = (Get-Item -Path WSMan:\localhost\Client\TrustedHosts).Value
+
+#What's in the variable?
+$hosts
+
+Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Concatenate 172.16.1.201 -Force
